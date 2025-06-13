fe15ba65b8756f78aa39105f7947c0b5655ac052
Amadeus Mader <amadeus@mozart409.com>
2025-06-13 22:47:16 +0200

    fix: :wip: changed guiAddress

--- systems/pinchflat/configuration.nix
+++ systems/pinchflat/configuration.nix
@@ -86,7 +86,8 @@
 
   services.syncthing = {
     enable = true;
-    guiAddress = "0.0.0.0:8384";
+    # guiAddress = "0.0.0.0:8384";
+    guiAddress = "127.0.0.1:8384";
     openDefaultPorts = true;
     systemService = true;
     overrideDevices = false;
